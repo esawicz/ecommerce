af3e66a1a9917f00590bebe122fe06b1f8ba2272
Ellen Sawicz <ellensawicz@Ellens-MacBook-Pro.local>
2016-11-15 20:43:05 -0700

    updated cartCtrl and totals

--- public/js/cartCtrl.js
+++ public/js/cartCtrl.js
@@ -3,17 +3,18 @@ angular.module('pink')
 
 
 	$scope.showCart = function() {
-			mainSrv.showCart().then(function(response){
-				$scope.cart= response;
-			})
-		}
+		mainSrv.showCart().then(function(response){
+			$scope.cart = response;
+			calcPriceInfo();
+		});
+	}
 
 	$scope.showCart();
 
 	$scope.clearItem = function(item) {
 		mainSrv.clearItem(item).then(function(response){
 			$scope.cart = response;
-		})
+		});
 	}
 
 	$scope.increaseQuantity = function (item) {
@@ -34,6 +35,34 @@ angular.module('pink')
 	function updateCartItem(item) {
 		mainSrv.updateCartItem(item).then(function (response) {
 			$scope.cart = response;
+			calcPriceInfo();
 		});
 	}
-})
+
+	function calcSubTotal() {
+		var total = 0;
+		for (var i = 0; i < $scope.cart.length; i++) {
+			total += $scope.cart[i].price * $scope.cart[i].quantity
+		}
+		$scope.subTotal = Math.round(total* 100) / 100;
+	}
+
+	function calcTax() {
+		var tax = $scope.subTotal * .07;
+	 	$scope.tax = Math.round(tax * 100)/100;
+ }
+
+ function calcTotal() {
+	 var total = $scope.subTotal + $scope.tax;
+	 $scope.total = Math.round(total * 100)/100;
+ }
+
+ function calcPriceInfo() {
+	 calcSubTotal();
+	 calcTax();
+	 calcTotal();
+ }
+
+
+
+});
